ec07b526dff8811446aff728fc510e53a082b474
Wildan <asep.wildan@lumoshive.com>
2024-07-11 09:56:44 +0700

    Sistem navigasi telah disesuaikan dengan fungsinya

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -275,11 +275,7 @@
                                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                 {{ __('Profile') }}
                             </a>
-                            <a class="dropdown-item" href="javascript:void(0)">
-                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                {{ __('Settings') }}
-                            </a>
-                            <a class="dropdown-item" href="javascript:void(0)">
+                            <a class="dropdown-item" href="{{route('logs.index')}}">
                                 <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                                 {{ __('Activity Log') }}
                             </a>
